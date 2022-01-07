974a1897fd81c4af8e7c838b7c570e68e4e4744a
Admin <admin@192.168.1.4>
2022-01-07 16:57:02 +0530

    sf

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,15 +1,11 @@
-// https://eth-ropsten.alchemyapi.io/v2/jFGhVfO7-hrXLNcocOOGYd0L-Pfdp187
-
 require('@nomiclabs/hardhat-waffle')
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/jFGhVfO7-hrXLNcocOOGYd0L-Pfdp187',
-      accounts: [
-        '8ee3627f76dd151d989510092f2596c4b7b8e156acfe9223a346a1d0258ee49b',
-      ],
+      url: 'Your live ropsten network url',
+      accounts: ['Your metamask private key'],
     },
   },
 }
